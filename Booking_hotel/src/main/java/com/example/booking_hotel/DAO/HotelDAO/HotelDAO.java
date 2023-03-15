1aeabf3e539bfff033afaf304c9db92def402171
khvu443 <khvu443@gmail.com>
2023-03-15 23:34:00 +0700

    Book hotel

---
+++ Booking_hotel/src/main/java/com/example/booking_hotel/DAO/HotelDAO/HotelDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.booking_hotel.DAO.HotelDAO;
+
+/**
+ *
+ * @author lenovo
+ */
+public class HotelDAO {
+    
+}
---
+++ Booking_hotel/src/main/java/com/example/booking_hotel/Service/HotelService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.booking_hotel.Service;
+
+/**
+ *
+ * @author lenovo
+ */
+public class HotelService {
+    
+}

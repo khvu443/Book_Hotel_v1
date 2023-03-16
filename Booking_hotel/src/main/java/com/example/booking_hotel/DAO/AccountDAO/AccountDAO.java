534f31cccb3774ebe667c7ae812139fbef6dcfb5
TonyToan <toantdkde160402@fpt.edu.vn>
2023-03-16 13:45:42 +0700

    them dbcontext - test connection db success - them dependencies sqljdbc42

--- Booking_hotel/src/main/java/com/example/booking_hotel/DAO/AccountDAO/AccountDAO.java
+++ Booking_hotel/src/main/java/com/example/booking_hotel/DAO/AccountDAO/AccountDAO.java
@@ -4,10 +4,43 @@
  */
 package com.example.booking_hotel.DAO.AccountDAO;
 
+import com.example.booking_hotel.Model.Account;
+import com.example.booking_hotel.context.DBContext;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author lenovo
  */
 public class AccountDAO {
-    
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    public List<Account> getAccountList() {
+        List<Account> list = new ArrayList<>();
+        String query = "SELECT * from [Account]";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Account(
+                                rs.getInt(1),
+                                rs.getString(2),
+                                rs.getString(3),
+                                rs.getString(4),
+                                rs.getInt(5),
+                                rs.getInt(6))
+
+                );
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
 }
